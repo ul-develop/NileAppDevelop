b4f588eb051ddb4cd41817e4525ef3e0c8b51b5a
Xiaoma Luke <xiaoma.luke@gmail.com>
2020-08-10 17:07:48 +0900

    78d4489 Merge pull request #51 from ul-develop/system_and_features  Implement System and features

--- search/service-worker.js
+++ search/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.14cfaac79d141b6c5ce87bb4c5a34052.js"
+  "./precache-manifest.affb4a05ba7cc8d2621af942127fb122.js"
 );
 
 self.addEventListener('message', (event) => {
